2c2ccce3cebc928e1135180dfa6a0d59dde8ee24
Arianna Giannoccaro <123045858@upq.edu.mx>
2025-01-22 14:42:35 -0600

    Estructura de un archivo JAVA y manejo de cadenas

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,9 +1,31 @@
+
+//IMPORTACIONES
+
+//Clase Principal (ejecuciones que yo quiera hacer)
+//Metodo Main (van dentro de las clases)
 public class Main {
 
+    /*soy
+    un
+    comentario
+    de varias
+    lineas
+     */
     public static void main(String[] args) {
 
-
+//        2. cadenas con JAVA
         System.out.println("Hello, Arianna Giannoccaro!");
-        
+        String cadenas = "arianna" + "giannoccaro" + "quiñonez";
+
+        System.out.println(cadenas);
+
+        System.out.println(cadenas.length());
+
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(2));
+        System.out.println(cadenas.substring(0,5));
+
     }
+
+
 }
