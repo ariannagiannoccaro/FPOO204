280ddf48b41c42434fe6e94067c5837cd25b21e1
Arianna Giannoccaro <123045858@upq.edu.mx>
2025-01-27 14:56:51 -0600

    programa 2
    Codifica un programa que pregunte el nombre completo del usuario en la
    consola y después muestre por pantalla el nombre completo del usuario tres
    veces, una con todas las letras minúsculas, otra con todas las letras mayúsculas y
    otra solo con la
    primera letra del nombre y de los apellidos en mayúscula. El usuario puede
    introducir su nombre combinando mayúsculas y minúsculas como quiera.

--- Practica no.3/src/Main.java
+++ Practica no.3/src/Main.java
@@ -4,13 +4,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scn = new Scanner(System.in);
+        /*Scanner scn = new Scanner(System.in);
         System.out.println("Ingrese Numero de horas trabajadas: ");
         int horas = scn.nextInt();
         System.out.println("Ingrese el costo por hora trabajada: ");
         int costo = scn.nextInt();
 
         int pagar = horas * costo;
-        System.out.println("El pago es: " + pagar);
+        System.out.println("El pago es: " + pagar);*/
+
+        //programa 2
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("Ingrese su apellido paterno: ");
+        String aP = sc.nextLine();
+        System.out.println("Ingrese su apellido Materno: ");
+        String aM = sc.nextLine();
+
+        System.out.println("Escribiré tu nombre todo en minusculas: " + nombre.toLowerCase() + aP.toLowerCase() + aM.toLowerCase());
+        System.out.println("Escribiré tu nombre todo en mayusculas: " + nombre.toUpperCase() + aP.toUpperCase() + aM.toUpperCase());
+        System.out.println(nombre.substring(0,1).toUpperCase() + nombre.substring(1).toLowerCase()
+                + aP.substring(0,1).toUpperCase() + aP.substring(1).toLowerCase()
+                + aM.substring(0,1).toUpperCase() + aM.substring(1).toLowerCase());
     }
 }
