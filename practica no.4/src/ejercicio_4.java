608ff3f1b68ebd5463c8de22104eb5db099771fa
Arianna Giannoccaro <123045858@upq.edu.mx>
2025-01-28 16:18:13 -0600

    ejercicio 4
    Codifica un programa que solicite una cadena de caracteres e imprima como
    resultado si la cadena es palíndromo o no

---
+++ practica no.4/src/ejercicio_4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio_4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce una cadena de texto: ");
+        String text = scanner.nextLine();
+
+        String reversedText = new StringBuilder(text).reverse().toString();
+
+        if (text.equalsIgnoreCase(reversedText)) {
+            System.out.println("La cadena es un palíndromo.");
+        } else {
+            System.out.println("La cadena no es un palíndromo.");
+        }
+
+    }
+}
