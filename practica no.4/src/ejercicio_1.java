2155438043efbbfcb23468a36cded0a77b78b6a6
Arianna Giannoccaro <123045858@upq.edu.mx>
2025-01-28 16:08:53 -0600

    Ejercicio 1
    Programa que almacena la cadena de caracteres una contraseña en una variable, después que solicite al usuario una contraseña e imprima en pantalla si la contraseña introducida por el usuario coincide con la guardada en
    la variable sin tener en cuenta mayúscula y minúscula.

---
+++ practica no.4/src/ejercicio_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ejercicio_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String password = "Sistemas204";
+
+        System.out.print("Introduce la contraseña: ");
+        String userInput = scanner.nextLine();
+
+        if (password.equalsIgnoreCase(userInput)) {
+            System.out.println("La contraseña es correcta.");
+        } else {
+            System.out.println("La contraseña es incorrecta.");
+        }
+    }
+}
