b2a47b113ebd010ec70858b058cfc0154b1f3390
Arianna Giannoccaro <123045858@upq.edu.mx>
2025-03-25 16:41:41 -0600

    practica no.17

--- CrudUser204/src/UserCRUD.java
+++ CrudUser204/src/UserCRUD.java
@@ -11,6 +11,7 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 
 public class UserCRUD {
@@ -39,6 +40,34 @@ public class UserCRUD {
             System.out.println("Error al crear el usuario "+ e.getMessage());//retornar el contido del error y cual era el error
             return false;
         }
-    }
+    }//fin del Insert
     
+    //método: consultar por ID
+    
+    public ResultSet buscarPorID (int id){
+        String sqlBuscar = "SELECT * FROM usuarios WHERE id= ?";
+        
+        try{
+            PreparedStatement ps= conexion.prepareStatement(sqlBuscar);
+            ps.setInt(1, id);
+            return ps.executeQuery();
+        }
+        catch(SQLException e){
+            System.out.println("Error al buscar por Id "+ e.getMessage());//retornar el contido del error y cual era el error
+            return null;
+        }
+    }// fin de CONSULTA ID
+    
+    public ResultSet obtenerTodos (){
+        String sqlTodos = "SELECT * FROM usuarios";
+        
+        try{
+            PreparedStatement ps= conexion.prepareStatement(sqlTodos);
+            return ps.executeQuery();
+        }
+        catch(SQLException s){
+            System.out.println("Error al consultar todos"+ s.getMessage());
+            return null;
+        }
+    }// fin obtenerTodos
 }
